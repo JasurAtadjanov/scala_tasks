99ed79489d537f155797c1fdcfb9fce199651bfc
Jasur Atadjanov <jatadjanov@exadel.com>
2021-04-26 12:17:45 +0500

    Task3: replace if to match

--- main/scala/Task3.scala
+++ main/scala/Task3.scala
@@ -7,21 +7,18 @@ object Task3 {
   def encodeDirect(str: List[Char]): List[(Int, Char)] = {
     assert(str.nonEmpty, "Please enter symbols")
 
-    var result: ArrayBuffer[(Int, Char)] = empty
+    val result: ArrayBuffer[(Int, Char)] = empty
     val n = str.length - 1
 
     def f (idx: Int, ch: Char, cnt: Int): Unit = {
-      if (idx == n){
-        if (ch == str(idx))
-          result.append((cnt + 1, ch))
-        else {
+      idx == n match {
+        case true if ch == str(idx) => result.append((cnt + 1, ch))
+        case true => {
           result.append((cnt, ch))
           result.append((1, str(idx)))
         }
-      } else {
-        if (ch == str(idx))
-          f(idx + 1, ch, cnt + 1)
-        else {
+        case false if ch == str(idx) => f(idx + 1, ch, cnt + 1)
+        case false => {
           result.append((cnt, ch))
           f(idx + 1, str(idx), 1)
         }
@@ -33,6 +30,6 @@ object Task3 {
   }
 
   def main(args: Array[String]): Unit = {
-    println(encodeDirect("aaaabbbcaat".toList))
+    println(encodeDirect("aaaabbbcaatt".toList))
   }
 }
